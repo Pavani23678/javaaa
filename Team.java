d1e4efb10ca32bd64b41701dabf611f44c1815da
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-07-14 15:15:19 +0530

    source

---
+++ Team.java
@@ -0,0 +1,9 @@
+public class Team {
+    public void name(){
+        System.out.println("Pavani");
+        System.out.println("Ganesh");
+        System.out.println("sankeerthana");
+        System.out.println("syam");
+    }
+    
+}
